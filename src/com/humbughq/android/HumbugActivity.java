5011afb4cff68a4716bb4cb4602240e6f4c5baf8
Luke Faraone <lfaraone@humbughq.com>
2012-10-16 16:30:40 -0400

    Temporarily rip out gravatar support.

--- src/com/humbughq/android/HumbugActivity.java
+++ src/com/humbughq/android/HumbugActivity.java
@@ -1,19 +1,14 @@
 package com.humbughq.android;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.Activity;
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -50,25 +45,6 @@ public class HumbugActivity extends Activity {
         leftTile.addView(stream2);
         rightTile.addView(instance);
 
-        AssetManager am = getAssets();
-
-        ImageView gravatarView = new ImageView(this);
-        Bitmap gravatar = profile_pictures.get(message.getSenderEmail());
-
-        if (gravatar == null) {
-            BufferedInputStream buf;
-            try {
-                // TODO don't use a static file
-                buf = new BufferedInputStream(am.open("sample.png"));
-                gravatar = BitmapFactory.decodeStream(buf);
-                profile_pictures.put(message.getSenderEmail(), gravatar);
-            } catch (IOException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-        }
-
-        gravatarView.setImageBitmap(gravatar);
         TextView senderName = new TextView(this);
         senderName.setWidth(100);
         senderName.setGravity(Gravity.CENTER_HORIZONTAL);
@@ -78,7 +54,6 @@ public class HumbugActivity extends Activity {
         String content = message.getContent().replaceAll("\\<.*?>", "");
         contentView.setText(content);
         contentView.setPadding(10, 0, 10, 10);
-        leftTile.addView(gravatarView);
         leftTile.addView(senderName);
         rightTile.addView(contentView);
 
