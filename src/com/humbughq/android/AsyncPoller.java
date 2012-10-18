7e173cfe8b7ffc4e0531d0c8a9a00127caf7c350
Luke Faraone <lfaraone@humbughq.com>
2012-10-18 14:45:59 -0400

    Handle null replies.

--- src/com/humbughq/android/AsyncPoller.java
+++ src/com/humbughq/android/AsyncPoller.java
@@ -19,23 +19,26 @@ class AsyncPoller extends HumbugAsyncPushTask {
     @Override
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
+        if (result != null) {
+            try {
+                JSONObject response = new JSONObject(result);
+                JSONArray objects = response.getJSONArray("messages");
+                for (int i = 0; i < objects.length(); i++) {
+                    Log.i("json-iter", "" + i);
+                    Message message = new Message(objects.getJSONObject(i));
+                    this.that.messages.add(message);
 
-        try {
-            JSONObject response = new JSONObject(result);
-            JSONArray objects = response.getJSONArray("messages");
-            for (int i = 0; i < objects.length(); i++) {
-                Log.i("json-iter", "" + i);
-                Message message = new Message(objects.getJSONObject(i));
-                this.that.messages.add(message);
-
-                if (message.getType() == Message.STREAM_MESSAGE) {
-                    this.that.tilepanel.addView(this.that
-                            .renderStreamMessage(message));
+                    if (message.getType() == Message.STREAM_MESSAGE) {
+                        this.that.tilepanel.addView(this.that
+                                .renderStreamMessage(message));
+                    }
                 }
+            } catch (JSONException e) {
+                Log.e("json", "parsing error");
+                e.printStackTrace();
             }
-        } catch (JSONException e) {
-            Log.e("json", "parsing error");
-            e.printStackTrace();
+        } else {
+            Log.i("poll", "got nothing from the server");
         }
         if (this.that.suspended) {
             Log.i("poll", "suspended, dying");
