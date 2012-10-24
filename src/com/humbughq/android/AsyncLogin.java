e8905b982c980951104bca447a27fc6f45bdc015
Luke Faraone <lfaraone@humbughq.com>
2012-10-24 15:08:30 -0400

    Remove duplicate definition of reference.

--- src/com/humbughq/android/AsyncLogin.java
+++ src/com/humbughq/android/AsyncLogin.java
@@ -4,7 +4,6 @@ import android.util.Log;
 import android.widget.TextView;
 
 class AsyncLogin extends HumbugAsyncPushTask {
-    HumbugActivity that;
 
     public AsyncLogin(HumbugActivity humbugActivity, String username,
             String password) {
