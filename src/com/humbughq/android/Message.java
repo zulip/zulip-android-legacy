9cb860c1a3dc605ed0145464cead9dd514fa7fe0
Luke Faraone <lfaraone@humbughq.com>
2012-11-19 17:43:36 -0500

    Comment my code.

--- src/com/humbughq/android/Message.java
+++ src/com/humbughq/android/Message.java
@@ -21,12 +21,34 @@ public class Message {
     private int id;
     private String your_email;
 
+    /**
+     * Construct a new Message from JSON returned by the Humbug server.
+     * 
+     * @param context
+     *            The HumbugActivity that created the message
+     * @param message
+     *            The JSON object parsed from the server's output
+     * @throws JSONException
+     *             Thrown if the JSON provided is malformed.
+     */
     public Message(HumbugActivity context, JSONObject message)
             throws JSONException {
         this.your_email = context.email;
         this.populate(message);
     }
 
+    /**
+     * Construct a new Message from JSON returned by the Humbug server.
+     * 
+     * This method operates without HumbugActivity context, so some automatic
+     * features like excluding the user from the recipient list are not
+     * performed.
+     * 
+     * @param message
+     *            The JSON object parsed from the server's output
+     * @throws JSONException
+     *             Thrown if the JSON provided is malformed.
+     */
     public Message(JSONObject message) throws JSONException {
         this.populate(message);
     }
@@ -35,6 +57,15 @@ public class Message {
         // Dummy empty constructor
     }
 
+    /**
+     * Convenience function to return either the Recipient specified or the
+     * sender of the message as appropriate.
+     * 
+     * @param other
+     *            a Recipient object you want to analyse
+     * @return Either the specified Recipient's full name, or the sender's name
+     *         if you are the Recipient.
+     */
     private String getNotYouRecipient(JSONObject other) {
         try {
             if (!other.getString("email").equals(this.your_email)) {
@@ -49,6 +80,13 @@ public class Message {
         return null;
     }
 
+    /**
+     * Populate a Message object based off a parsed JSON hash.
+     * 
+     * @param message
+     *            the JSON object as returned by the server.
+     * @throws JSONException
+     */
     public void populate(JSONObject message) throws JSONException {
         this.setSender(message.getString("sender_full_name"));
         this.setSenderEmail(message.getString("sender_email"));
@@ -95,6 +133,12 @@ public class Message {
         this.recipients = recipients;
     }
 
+    /**
+     * Convenience function to set the recipient in case of a single recipient.
+     * 
+     * @param recipient
+     *            The sole recipient of the message.
+     */
     public void setRecipient(String recipient) {
         String[] recipients = new String[1];
         recipients[0] = recipient;
