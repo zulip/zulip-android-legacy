e0fd7fe5f4fa8376cb85ac244332f67243743eda
Luke Faraone <lfaraone@humbughq.com>
2012-11-28 15:23:50 -0500

    Refactor out tile generation for speed improvements.
    This way we reuse tiles if they already exist, and create a blank one
    if they don't.
    
    I smell an intern project to refactor these into a layout.

--- src/com/humbughq/android/MessageAdapter.java
+++ src/com/humbughq/android/MessageAdapter.java
@@ -18,69 +18,104 @@ public class MessageAdapter extends ArrayAdapter<Message> {
         super(context, 0, objects);
     }
 
-    public View getView(int position, View convertView, ViewGroup ignored) {
-        LinearLayout tile;
-
+    private LinearLayout generateTile() {
         Context context = this.getContext();
-        Message message = getItem(position);
 
-        if (convertView != null) {
-            tile = (LinearLayout) convertView;
-            tile.removeAllViews();
-        } else {
-            tile = new LinearLayout(context);
-        }
-
+        LinearLayout tile = new LinearLayout(context);
         tile.setOrientation(LinearLayout.VERTICAL);
 
         LinearLayout envelopeTile = new LinearLayout(context);
         envelopeTile.setOrientation(LinearLayout.HORIZONTAL);
-        if (message.getType() == MessageType.STREAM_MESSAGE) {
-            envelopeTile.setBackgroundResource(R.drawable.stream_header);
-        } else {
-            envelopeTile.setBackgroundResource(R.drawable.huddle_header);
-        }
+        envelopeTile.setTag("envelope");
 
         tile.addView(envelopeTile);
 
         TextView display_recipient = new TextView(context);
-        if (message.getType() != MessageType.STREAM_MESSAGE) {
-            display_recipient.setText(context.getString(R.string.huddle_text)
-                    + " " + message.getDisplayRecipient());
-            display_recipient.setTextColor(Color.WHITE);
-        } else {
-            display_recipient.setText(message.getDisplayRecipient());
-        }
+        display_recipient.setTag("display_recipient");
+
         display_recipient.setTypeface(Typeface.DEFAULT_BOLD);
         display_recipient.setGravity(Gravity.CENTER_HORIZONTAL);
         display_recipient.setPadding(10, 5, 10, 5);
 
         envelopeTile.addView(display_recipient);
 
-        if (message.getType() == MessageType.STREAM_MESSAGE) {
-            TextView sep = new TextView(context);
-            sep.setText(" | ");
+        TextView sep = new TextView(context);
+        sep.setText(" | ");
+        sep.setTag("sep");
 
-            TextView instance = new TextView(context);
-            instance.setText(message.getSubject());
-            instance.setPadding(10, 5, 10, 5);
+        TextView instance = new TextView(context);
+        instance.setPadding(10, 5, 10, 5);
+        instance.setTag("instance");
 
-            envelopeTile.addView(sep);
-            envelopeTile.addView(instance);
-        }
+        envelopeTile.addView(sep);
+        envelopeTile.addView(instance);
 
         TextView senderName = new TextView(context);
 
-        senderName.setText(message.getSender().getName());
+        senderName.setTag("senderName");
         senderName.setPadding(10, 5, 10, 5);
         senderName.setTypeface(Typeface.DEFAULT_BOLD);
 
         tile.addView(senderName);
 
         TextView contentView = new TextView(context);
-        String content = message.getContent();
-        contentView.setText(content);
+        contentView.setTag("contentView");
         contentView.setPadding(10, 0, 10, 10);
+
+        tile.addView(contentView);
+
+        return tile;
+    }
+
+    public View getView(int position, View convertView, ViewGroup ignored) {
+
+        Context context = this.getContext();
+        Message message = getItem(position);
+        LinearLayout tile;
+
+        if (convertView == null) {
+            tile = generateTile();
+        } else {
+            tile = (LinearLayout) convertView;
+        }
+
+        LinearLayout envelopeTile = (LinearLayout) tile
+                .findViewWithTag("envelope");
+        TextView display_recipient = (TextView) tile
+                .findViewWithTag("display_recipient");
+
+        if (message.getType() == MessageType.STREAM_MESSAGE) {
+            envelopeTile.setBackgroundResource(R.drawable.stream_header);
+        } else {
+            envelopeTile.setBackgroundResource(R.drawable.huddle_header);
+        }
+
+        if (message.getType() != MessageType.STREAM_MESSAGE) {
+            display_recipient.setText(context.getString(R.string.huddle_text)
+                    + " " + message.getDisplayRecipient());
+            display_recipient.setTextColor(Color.WHITE);
+        } else {
+            display_recipient.setText(message.getDisplayRecipient());
+            display_recipient.setTextColor(Color.BLACK);
+        }
+
+        TextView sep = (TextView) tile.findViewWithTag("sep");
+        TextView instance = (TextView) tile.findViewWithTag("instance");
+
+        if (message.getType() == MessageType.STREAM_MESSAGE) {
+            instance.setVisibility(View.VISIBLE);
+            sep.setVisibility(View.VISIBLE);
+            instance.setText(message.getSubject());
+        } else {
+            instance.setVisibility(View.GONE);
+            sep.setVisibility(View.GONE);
+        }
+
+        TextView senderName = (TextView) tile.findViewWithTag("senderName");
+        senderName.setText(message.getSender().getName());
+
+        TextView contentView = (TextView) tile.findViewWithTag("contentView");
+        contentView.setText(message.getContent());
 
         int color = Color.WHITE;
 
@@ -93,10 +128,10 @@ public class MessageAdapter extends ArrayAdapter<Message> {
         senderName.setBackgroundColor(color);
         contentView.setBackgroundColor(color);
 
-        tile.addView(contentView);
         tile.setTag(R.id.messageID, message.getID());
 
         return tile;
+
     }
 
     public long getItemId(int position) {
