12a8b2c06db583663f47bbd612dfde52bbad4d39
Scott Feeney <scott@zulip.com>
2013-08-15 14:37:18 -0400

    Make HumbugActivity.onCreate protected

--- src/com/humbughq/mobile/HumbugActivity.java
+++ src/com/humbughq/mobile/HumbugActivity.java
@@ -70,7 +70,7 @@ public class HumbugActivity extends Activity {
 
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         app = (ZulipApp) getApplicationContext();
         settings = app.settings;
