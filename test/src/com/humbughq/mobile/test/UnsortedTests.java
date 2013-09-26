2ca451d037f45e265e82278b1344d623a33d8b54
Luke Faraone <lfaraone@zulip.com>
2013-09-25 22:20:18 -0400

    Apply formatter to testcases

--- test/src/com/humbughq/mobile/test/UnsortedTests.java
+++ test/src/com/humbughq/mobile/test/UnsortedTests.java
@@ -23,14 +23,17 @@ public class UnsortedTests extends ActivityUnitTestCase<HumbugActivity> {
         super.setUp();
         app = new ZulipApp();
     }
-    
+
     public void testMessageCreation() throws JSONException {
         testInit();
         JSONObject jsonObject;
-        jsonObject = new JSONObject("{\"recipient_id\": 314, \"sender_email\": \"lfaraone@zulip.com\", \"timestamp\": 1379966441, \"display_recipient\": \"test\", \"sender_id\": 15, \"sender_full_name\": \"Luke Faraone\", \"sender_domain\": \"zulip.com\", \"content\": \"Hello!\", \"gravatar_hash\": \"9cd8e1981dc89f3221c5077dd8a22515\", \"avatar_url\": \"https://secure.gravatar.com/avatar/9cd8e1981dc89f3221c5077dd8a22515?d=identicon\", \"client\": \"website\", \"content_type\": \"text/x-markdown\", \"subject_links\": [], \"sender_short_name\": \"lfaraone\", \"type\": \"stream\", \"id\": 10594623, \"subject\": \"toast\"}");
-        Message msg = new Message((ZulipApp) getActivity().getApplication(), jsonObject);
+        jsonObject = new JSONObject(
+                "{\"recipient_id\": 314, \"sender_email\": \"lfaraone@zulip.com\", \"timestamp\": 1379966441, \"display_recipient\": \"test\", \"sender_id\": 15, \"sender_full_name\": \"Luke Faraone\", \"sender_domain\": \"zulip.com\", \"content\": \"Hello!\", \"gravatar_hash\": \"9cd8e1981dc89f3221c5077dd8a22515\", \"avatar_url\": \"https://secure.gravatar.com/avatar/9cd8e1981dc89f3221c5077dd8a22515?d=identicon\", \"client\": \"website\", \"content_type\": \"text/x-markdown\", \"subject_links\": [], \"sender_short_name\": \"lfaraone\", \"type\": \"stream\", \"id\": 10594623, \"subject\": \"toast\"}");
+        Message msg = new Message((ZulipApp) getActivity().getApplication(),
+                jsonObject);
         assertEquals(msg.getID(), 10594623);
-        assertEquals(msg.getSender(), new Person("Luke Faraone", "lfaraone@zulip.com"));
+        assertEquals(msg.getSender(), new Person("Luke Faraone",
+                "lfaraone@zulip.com"));
     }
 
     /**
@@ -38,11 +41,13 @@ public class UnsortedTests extends ActivityUnitTestCase<HumbugActivity> {
      */
     private void testInit() {
         setApplication(app);
-        this.startActivity(new Intent(getInstrumentation().getTargetContext(), HumbugActivity.class), null, null);
+        this.startActivity(new Intent(getInstrumentation().getTargetContext(),
+                HumbugActivity.class), null, null);
         this.getInstrumentation().waitForIdleSync();
         app.setContext(getInstrumentation().getTargetContext());
-        ((ZulipApp) getActivity().getApplication()).setEmail("testuser@example.com");
-        
+        ((ZulipApp) getActivity().getApplication())
+                .setEmail("testuser@example.com");
+
     }
 
     protected void tearDown() throws Exception {
