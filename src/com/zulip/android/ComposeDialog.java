a3588bc68e55f5aba4c341230fd36523a29d4111
George Schneeloch <gschneeloch@zulip.com>
2014-01-25 23:45:14 -0500

    Add autocomplete for compose fields. (This doesn't include multiple email addresses yet)

--- src/com/zulip/android/ComposeDialog.java
+++ src/com/zulip/android/ComposeDialog.java
@@ -2,18 +2,27 @@ package com.zulip.android;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.FilterQueryProvider;
 
+import com.j256.ormlite.android.AndroidDatabaseResults;
 import com.zulip.android.ZulipAsyncPushTask.AsyncTaskCompleteListener;
+
+import java.sql.SQLException;
 
 public class ComposeDialog extends DialogFragment {
     ZulipActivity activity;
@@ -21,8 +30,8 @@ public class ComposeDialog extends DialogFragment {
     private MessageType type;
 
     private View view;
-    private EditText recipient;
-    private EditText subject;
+    private AutoCompleteTextView recipient;
+    private AutoCompleteTextView subject;
     private EditText body;
 
     public static ComposeDialog newInstance(MessageType type, String stream,
@@ -95,8 +104,9 @@ public class ComposeDialog extends DialogFragment {
         Log.i("onCreateDialog", "" + type + " " + stream + " " + topic + " "
                 + pmRecipients);
 
-        recipient = (EditText) view.findViewById(R.id.composeRecipient);
-        subject = (EditText) view.findViewById(R.id.composeSubject);
+        recipient = (AutoCompleteTextView) view
+                .findViewById(R.id.composeRecipient);
+        subject = (AutoCompleteTextView) view.findViewById(R.id.composeSubject);
         body = (EditText) view.findViewById(R.id.composeText);
 
         activity = ((ZulipActivity) getActivity());
@@ -108,8 +118,75 @@ public class ComposeDialog extends DialogFragment {
 
         if (type == MessageType.STREAM_MESSAGE) {
             this.switchToStream();
+            try {
+                Cursor streamCursor = makeStreamCursor("");
+                SimpleCursorAdapter recipientAdapter = new SimpleCursorAdapter(
+                        getActivity(), R.layout.stream_tile, streamCursor,
+                        new String[] { Stream.NAME_FIELD },
+                        new int[] { R.id.name }, 0);
+                recipientAdapter
+                        .setCursorToStringConverter(new SimpleCursorAdapter.CursorToStringConverter() {
+                            @Override
+                            public CharSequence convertToString(Cursor cursor) {
+                                int index = cursor
+                                        .getColumnIndex(Stream.NAME_FIELD);
+                                return cursor.getString(index);
+                            }
+                        });
+                recipient.setAdapter(recipientAdapter);
+                recipientAdapter
+                        .setFilterQueryProvider(new FilterQueryProvider() {
+                            @Override
+                            public Cursor runQuery(CharSequence charSequence) {
+                                try {
+                                    return makeStreamCursor(charSequence);
+                                } catch (SQLException e) {
+                                    ZLog.logException(e);
+                                    // TODO: is there something like
+                                    // Cursor.empty()?
+                                    return null;
+                                }
+                            }
+                        });
+            } catch (SQLException e) {
+                ZLog.logException(e);
+            }
+            try {
+                Cursor subjectCursor = makeSubjectCursor("");
+                SimpleCursorAdapter subjectAdapter = new SimpleCursorAdapter(
+                        getActivity(), R.layout.stream_tile, subjectCursor,
+                        new String[] { Message.SUBJECT_FIELD },
+                        new int[] { R.id.name }, 0);
+                subjectAdapter
+                        .setCursorToStringConverter(new SimpleCursorAdapter.CursorToStringConverter() {
+                            @Override
+                            public CharSequence convertToString(Cursor cursor) {
+                                int index = cursor
+                                        .getColumnIndex(Message.SUBJECT_FIELD);
+                                return cursor.getString(index);
+                            }
+                        });
+                subjectAdapter
+                        .setFilterQueryProvider(new FilterQueryProvider() {
+                            @Override
+                            public Cursor runQuery(CharSequence charSequence) {
+                                try {
+                                    return makeSubjectCursor(charSequence);
+                                } catch (SQLException e) {
+                                    ZLog.logException(e);
+                                    // TODO: is there something like
+                                    // Cursor.empty()?
+                                    return null;
+                                }
+                            }
+                        });
+                subject.setAdapter(subjectAdapter);
+            } catch (SQLException e) {
+                ZLog.logException(e);
+            }
             if (stream != null) {
                 recipient.setText(stream);
+
                 if (topic != null) {
                     subject.setText(topic);
                     body.requestFocus();
@@ -123,6 +200,37 @@ public class ComposeDialog extends DialogFragment {
             }
         } else {
             this.switchToPersonal();
+            try {
+                Cursor streamCursor = makePeopleCursor("");
+                SimpleCursorAdapter emailAdapter = new SimpleCursorAdapter(
+                        getActivity(), R.layout.stream_tile, streamCursor,
+                        new String[] { Person.EMAIL_FIELD },
+                        new int[] { R.id.name }, 0);
+                recipient.setAdapter(emailAdapter);
+                emailAdapter
+                        .setCursorToStringConverter(new SimpleCursorAdapter.CursorToStringConverter() {
+                            @Override
+                            public CharSequence convertToString(Cursor cursor) {
+                                int index = cursor
+                                        .getColumnIndex(Person.EMAIL_FIELD);
+                                return cursor.getString(index);
+                            }
+                        });
+                emailAdapter.setFilterQueryProvider(new FilterQueryProvider() {
+                    @Override
+                    public Cursor runQuery(CharSequence charSequence) {
+                        try {
+                            return makePeopleCursor(charSequence);
+                        } catch (SQLException e) {
+                            ZLog.logException(e);
+                            // TODO: is there something like Cursor.empty()?
+                            return null;
+                        }
+                    }
+                });
+            } catch (SQLException e) {
+                ZLog.logException(e);
+            }
             if (pmRecipients != null) {
                 recipient.setText(pmRecipients);
                 body.requestFocus();
@@ -136,6 +244,50 @@ public class ComposeDialog extends DialogFragment {
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
         return dialog;
+    }
+
+    private Cursor makeStreamCursor(CharSequence match) throws SQLException {
+        if (match == null) {
+            match = "";
+        }
+        return ((AndroidDatabaseResults) app.getDao(Stream.class)
+                .queryBuilder().selectRaw("rowid _id", "*")
+                .orderByRaw(Stream.NAME_FIELD + " COLLATE NOCASE").where()
+                .eq(Stream.SUBSCRIBED_FIELD, true).and()
+                .like(Stream.NAME_FIELD, match + "%").queryRaw()
+                .closeableIterator().getRawResults()).getRawCursor();
+    }
+
+    private Cursor makeSubjectCursor(CharSequence match) throws SQLException {
+        if (match == null) {
+            match = "";
+        }
+        // _id must exist to use SimpleCursorAdapter but we can't use rowid
+        // because
+        // it would interfere with DISTINCT
+        AndroidDatabaseResults results = (AndroidDatabaseResults) app
+                .getDao(Message.class).queryBuilder().distinct()
+                .selectRaw("1 _id", Message.SUBJECT_FIELD)
+                .orderByRaw(Message.SUBJECT_FIELD + " COLLATE NOCASE").where()
+                .like(Message.SUBJECT_FIELD, match + "%").queryRaw()
+                .closeableIterator().getRawResults();
+        return results.getRawCursor();
+    }
+
+    private Cursor makePeopleCursor(CharSequence match) throws SQLException {
+        if (match == null) {
+            match = "";
+        }
+        // TODO: refactor this with the instance in ZulipActivity
+        Cursor peopleCursor = ((AndroidDatabaseResults) app
+                .getDao(Person.class).queryBuilder()
+                .selectRaw("rowid _id", "*")
+                .orderByRaw(Person.NAME_FIELD + " COLLATE NOCASE").where()
+                .eq(Person.ISBOT_FIELD, false).and()
+                .eq(Person.ISACTIVE_FIELD, true).and()
+                .like(Person.EMAIL_FIELD, match + "%").queryRaw()
+                .closeableIterator().getRawResults()).getRawCursor();
+        return peopleCursor;
     }
 
     private void sending(boolean isSending) {
