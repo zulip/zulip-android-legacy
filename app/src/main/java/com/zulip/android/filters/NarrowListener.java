7158cec83b9fd52999ffdd6df614c171032aad04
kunall17 <kunall.gupta17@gmail.com>
2016-04-08 16:27:15 +0530

    Added method to interface for MessageListFragment.

--- app/src/main/java/com/zulip/android/filters/NarrowListener.java
+++ app/src/main/java/com/zulip/android/filters/NarrowListener.java
@@ -1,8 +1,11 @@
 package com.zulip.android.filters;
 
+import com.zulip.android.models.Message;
+
 /**
  * Listener for narrow actions
  */
 public interface NarrowListener {
     void onNarrow(NarrowFilter narrowFilter);
+    void onNarrowFillSendBox(Message message);
 }
