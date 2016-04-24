62d08b126617dc18383af3d088e91f0d2ee4a0bd
niftynei <niftynei@gmail.com>
2016-04-24 17:33:11 -0400

    Fix logic error on logging to crashlytics

--- app/src/main/java/com/zulip/android/ZLog.java
+++ app/src/main/java/com/zulip/android/ZLog.java
@@ -1,12 +1,14 @@
 package com.zulip.android;
 
+import android.util.Log;
+
 import com.crashlytics.android.Crashlytics;
 
 public class ZLog {
 
     public static void logException(Throwable e) {
-        if (BuildHelper.shouldLogToCrashlytics()) {
-            e.printStackTrace();
+        if (!BuildHelper.shouldLogToCrashlytics()) {
+            Log.e("Error", "oops", e);
         } else {
             Crashlytics.logException(e);
         }
