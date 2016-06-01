12575fddc21e148de2d86e33336c8ed1f7f56447
kunall17 <kunall.gupta17@gmail.com>
2016-06-01 06:16:40 +0530

    Added click listener for devAuth link

--- app/src/main/java/com/zulip/android/activities/LoginActivity.java
+++ app/src/main/java/com/zulip/android/activities/LoginActivity.java
@@ -22,6 +22,7 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.zulip.android.BuildConfig;
 import com.zulip.android.R;
+import com.zulip.android.networking.AsyncDevGetEmails;
 import com.zulip.android.util.ZLog;
 import com.zulip.android.ZulipApp;
 import com.zulip.android.networking.ZulipAsyncPushTask.AsyncTaskCompleteListener;
@@ -124,7 +125,6 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
         }
 
         ((ZulipApp) getApplication()).setServerURL(serverUri.toString());
-        Toast.makeText(this, getString(R.string.logging_into_server, serverUri.toString()), Toast.LENGTH_SHORT).show();
     }
 
 
@@ -140,7 +140,7 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
                 return;
             }
 
-            final AsyncLogin loginTask = new AsyncLogin(LoginActivity.this, "google-oauth2-token", account.getIdToken());
+            final AsyncLogin loginTask = new AsyncLogin(LoginActivity.this, "google-oauth2-token", account.getIdToken(), false);
             loginTask.setCallback(new AsyncTaskCompleteListener() {
                 @Override
                 public void onTaskComplete(String result, JSONObject object) {
@@ -233,15 +233,17 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
             case R.id.google_sign_in_button:
                 connectionProgressDialog.show();
                 saveServerURL();
+                Toast.makeText(this, getString(R.string.logging_into_server, ZulipApp.get().getServerURI()), Toast.LENGTH_SHORT).show();
                 setupGoogleSignIn();
                 break;
             case R.id.zulip_login:
                 if (!isInputValid()) return;
                 saveServerURL();
+                Toast.makeText(this, getString(R.string.logging_into_server, ZulipApp.get().getServerURI()), Toast.LENGTH_SHORT).show();
                 connectionProgressDialog.show();
 
                 AsyncLogin alog = new AsyncLogin(LoginActivity.this,
-                        mUserName.getText().toString(), mPassword.getText().toString());
+                        mUserName.getText().toString(), mPassword.getText().toString(), false);
                 // Remove the CPD when done
                 alog.setCallback(new AsyncTaskCompleteListener() {
                     @Override
@@ -260,11 +262,35 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
             case R.id.legal_button:
                 openLegal();
                 break;
+            case R.id.local_server_button:
+                if (!isInputValidForDevAuth()) return;
+                saveServerURL();
+                AsyncDevGetEmails asyncDevGetEmails = new AsyncDevGetEmails(LoginActivity.this);
+                asyncDevGetEmails.execute();
             default:
                 break;
         }
     }
+    private boolean isInputValidForDevAuth() {
+        boolean isValid = true;
 
+        if (!mUseZulipCheckbox.isChecked()) {
+            if (mServerEditText.length() == 0) {
+                isValid = false;
+                mServerEditText.setError(getString(R.string.server_domain_required));
+            } else {
+                String serverString = mServerEditText.getText().toString();
+                if (!serverString.contains("://")) serverString = "https://" + serverString;
+
+                if (!Patterns.WEB_URL.matcher(serverString).matches()) {
+                    mServerEditText.setError(getString(R.string.invalid_domain));
+                    isValid = false;
+                }
+            }
+        }
+
+        return isValid;
+    }
     private boolean isInputValid() {
         boolean isValid = true;
 
