8cc08dc5b03dd51803b4c02dfa3876cae0523ef8
Kunal <kunall.gupta17@gmail.com>
2016-08-10 18:44:30 +0530

    Removed unused imports

--- app/src/main/java/com/zulip/android/activities/LoginActivity.java
+++ app/src/main/java/com/zulip/android/activities/LoginActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
--- app/src/main/java/com/zulip/android/viewholders/LoadingHolder.java
+++ app/src/main/java/com/zulip/android/viewholders/LoadingHolder.java
@@ -2,9 +2,6 @@ package com.zulip.android.viewholders;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.LinearLayout;
-
-import com.zulip.android.R;
 
 public class LoadingHolder extends RecyclerView.ViewHolder {
     public LoadingHolder(View footerView) {
