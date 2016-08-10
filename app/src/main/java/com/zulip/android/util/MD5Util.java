8f4af8756ce71c37f858eec72fc13a2b51b7eb33
Kunal <kunall.gupta17@gmail.com>
2016-08-10 18:47:07 +0530

    Change StringBuffer to StringBuilder

--- app/src/main/java/com/zulip/android/util/MD5Util.java
+++ app/src/main/java/com/zulip/android/util/MD5Util.java
@@ -15,7 +15,7 @@ public class MD5Util {
     private MD5Util() {}
 
     private static String hex(byte[] array) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < array.length; ++i) {
             sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(
                     1, 3));
