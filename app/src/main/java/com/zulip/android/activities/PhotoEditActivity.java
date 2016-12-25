ce03d80443c7ab9bd660964f1e7d5d49e68e0556
Sam <saumya.bhatnagar.sb@gmail.com>
2016-12-25 12:04:04 +0530

    Set up layout for PhotoEditActivity.

--- app/src/main/java/com/zulip/android/activities/PhotoEditActivity.java
+++ app/src/main/java/com/zulip/android/activities/PhotoEditActivity.java
@@ -2,6 +2,7 @@ package com.zulip.android.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import com.zulip.android.R;
 
@@ -11,5 +12,9 @@ public class PhotoEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_edit);
+
+        // run activity in full screen mode
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
