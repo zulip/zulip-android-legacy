41ceacd2522a4b727f7455b74afb4e55d2f401f3
Vishwesh <vishwesh03071998@gmail.com>
2017-01-19 12:27:12 +0530

    Show only messages of selected date.
    Changed where clause for database query and
    isSameDate check.
    
    Fixes #310.

---
+++ app/src/main/java/com/zulip/android/util/Constants.java
@@ -0,0 +1,10 @@
+package com.zulip.android.util;
+
+/**
+ * List of all Constants used in the projects
+ */
+
+public class Constants {
+    public static int MILLISECONDS_IN_A_MINUTE = 1000;
+    public static String DATE_FORMAT = "dd/MM/yyyy";
+}
