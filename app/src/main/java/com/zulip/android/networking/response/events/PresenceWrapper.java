1f9280f0fa49e15f0e70944d77261e4189ef8603
Sam <saumya.bhatnagar.sb@gmail.com>
2017-01-22 20:18:21 +0530

    Add documentation.

--- app/src/main/java/com/zulip/android/networking/response/events/PresenceWrapper.java
+++ app/src/main/java/com/zulip/android/networking/response/events/PresenceWrapper.java
@@ -3,7 +3,12 @@ package com.zulip.android.networking.response.events;
 import com.google.gson.annotations.SerializedName;
 import com.zulip.android.models.Presence;
 
-
+/**
+ * This class is used to deserialize the presence type event {@link EventsBranch.BranchType#PRESENCE}
+ *
+ * example : {"id":0,"server_timestamp":1485171896.1950330734,"type":"presence","email":"example@gmail.com",
+ *      "presence":{"ZulipAndroid":{"status":"active","timestamp":1485171896,"client":"ZulipAndroid","pushable":null}}}
+ */
 public class PresenceWrapper extends EventsBranch {
 
     @SerializedName("server_timestamp")
