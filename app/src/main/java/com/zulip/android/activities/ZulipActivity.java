0ff72d931166b7df361fee35dcc4a22882b17912
Sam <saumya.bhatnagar.sb@gmail.com>
2017-01-27 10:42:59 +0530

    Search in current narrowed view.
    Currently, search is performed in home view. This is especially
    undesirable when the query gives a lot of matching results. It'd be
    helpful to be able to search in current view of the user.

--- app/src/main/java/com/zulip/android/activities/ZulipActivity.java
+++ app/src/main/java/com/zulip/android/activities/ZulipActivity.java
@@ -1942,7 +1942,12 @@ public class ZulipActivity extends BaseActivity implements
             searchView.setOnQueryTextListener(new android.support.v7.widget.SearchView.OnQueryTextListener() {
                 @Override
                 public boolean onQueryTextSubmit(String s) {
-                    onNarrow(new NarrowFilterSearch(s));
+                    if (narrowedList != null) {
+                        onNarrow(new NarrowFilterSearch(s, narrowedList.filter));
+                    } else {
+                        onNarrow(new NarrowFilterSearch(s, null));
+                    }
+
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
                         mSearchMenuItem.collapseActionView();
                     }
@@ -2004,7 +2009,11 @@ public class ZulipActivity extends BaseActivity implements
                                 public void onClick(
                                         DialogInterface dialogInterface, int i) {
                                     String query = editText.getText().toString();
-                                    onNarrow(new NarrowFilterSearch(query));
+                                    if (narrowedList != null) {
+                                        onNarrow(new NarrowFilterSearch(query, narrowedList.filter));
+                                    } else {
+                                        onNarrow(new NarrowFilterSearch(query, null));
+                                    }
                                 }
                             });
                     builder.show();
