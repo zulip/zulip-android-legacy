2d4006e8970e352d8411c78facb6d71fa3064b06
Sam <saumya.bhatnagar.sb@gmail.com>
2017-02-07 09:13:28 +0530

    Add clickable spans for #stream-name.

---
+++ app/src/main/java/com/zulip/android/util/StreamSpan.java
@@ -0,0 +1,53 @@
+package com.zulip.android.util;
+
+import android.content.Context;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+import android.view.View;
+
+import com.zulip.android.ZulipApp;
+import com.zulip.android.models.Stream;
+
+/**
+ * Custom ClickableSpan to support #stream-name click. The user is taken to the last message read
+ * in the stream.
+ */
+
+public class StreamSpan extends ClickableSpan {
+    private String streamId;
+    private int color;
+
+    public StreamSpan(String streamId, int color) {
+        this.streamId = streamId;
+        this.color = color;
+    }
+
+    /**
+     * Performs the click action associated with this span.
+     */
+    @Override
+    public void onClick(View widget) {
+        Context context = widget.getContext().getApplicationContext();
+
+        // get stream name from streamId string
+        String streamName = null;
+        if (streamId != null) {
+            Stream stream = Stream.getById(ZulipApp.get(), Integer.parseInt(streamId));
+            if (stream != null) {
+                streamName = stream.getName();
+            }
+        }
+
+        // go to last message read in the stream
+        (((ZulipApp) context).getZulipActivity()).doNarrowToLastRead(streamName);
+    }
+
+    /**
+     * Makes the text underlined and in the link color.
+     */
+    @Override
+    public void updateDrawState(TextPaint ds) {
+        ds.setColor(this.color);
+        ds.setUnderlineText(false);
+    }
+}
