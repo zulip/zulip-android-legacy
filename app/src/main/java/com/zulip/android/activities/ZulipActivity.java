ead17e10348cb740e6a0130897da44da13e63983
Sam <saumya.bhatnagar.sb@gmail.com>
2017-02-14 19:23:14 +0530

    Apply markdown to uploaded links.

--- app/src/main/java/com/zulip/android/activities/ZulipActivity.java
+++ app/src/main/java/com/zulip/android/activities/ZulipActivity.java
@@ -1067,7 +1067,7 @@ public class ZulipActivity extends BaseActivity implements
      *
      * @param file on local storage
      */
-    private void uploadFile(File file) {
+    private void uploadFile(final File file) {
 
         // create RequestBody instance from file
         RequestBody requestFile =
@@ -1095,7 +1095,8 @@ public class ZulipActivity extends BaseActivity implements
                         sendingMessage(false, loadingMsg);
 
                         // print message to compose box
-                        messageEt.append(" " + UrlHelper.addHost(filePathOnServer));
+                        messageEt.append(" [" + file.getName() + "](" +
+                                UrlHelper.addHost(filePathOnServer) + ")");
                     } else {
                         // remove loading message from the screen
                         sendingMessage(false, loadingMsg);
