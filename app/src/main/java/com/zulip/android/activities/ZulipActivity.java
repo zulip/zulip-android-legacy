6e72c0771e84ef7d1401e7ddf36716e3902a0b58
Vishwesh <vishwesh03071998@gmail.com>
2017-02-26 04:02:53 +0530

    Added @-mentions option in people drawer.
    Fix:#351 and Fix:#39

--- app/src/main/java/com/zulip/android/activities/ZulipActivity.java
+++ app/src/main/java/com/zulip/android/activities/ZulipActivity.java
@@ -86,6 +86,7 @@ import com.zulip.android.database.DatabaseHelper;
 import com.zulip.android.filters.NarrowFilter;
 import com.zulip.android.filters.NarrowFilterAllPMs;
 import com.zulip.android.filters.NarrowFilterByDate;
+import com.zulip.android.filters.NarrowFilterMentioned;
 import com.zulip.android.filters.NarrowFilterPM;
 import com.zulip.android.filters.NarrowFilterSearch;
 import com.zulip.android.filters.NarrowFilterStar;
@@ -722,6 +723,8 @@ public class ZulipActivity extends BaseActivity implements
                     resetPeopleSearch();
                     if (id == Constants.ALL_PEOPLE_ID) {
                         openAllPrivateMessages();
+                    } else if (id == Constants.MENTIONS) {
+                        doNarrow(new NarrowFilterMentioned());
                     } else {
                         Person person = Person.getById(app, id);
                         narrowPMWith(person);
