8a6786a32f4d186d282982d592a996104adfb90c
Sam <saumya.bhatnagar.sb@gmail.com>
2017-03-11 07:47:39 +0530

    Fix: Use support library Dialog fragment.
    On pre marshmallow devices, fragment.onAttach(context) doesn't exist
    as it was added in api 23.

--- app/src/main/java/com/zulip/android/util/ListDialog.java
+++ app/src/main/java/com/zulip/android/util/ListDialog.java
@@ -2,7 +2,7 @@ package com.zulip.android.util;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
+import android.support.v4.app.DialogFragment;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
