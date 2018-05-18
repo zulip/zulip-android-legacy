7be541e1baf6b51d0bed946bf6832a425a26f73b
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-05-18 19:30:03 +0530

    update Dialog: Add preference to don't show again.

--- app/src/main/java/com/zulip/android/util/ShowAppUpdateDialog.java
+++ app/src/main/java/com/zulip/android/util/ShowAppUpdateDialog.java
@@ -7,14 +7,21 @@ import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 
 import com.zulip.android.R;
+import com.zulip.android.ZulipApp;
 
 
 public class ShowAppUpdateDialog {
 
     public static void showDialog(final Context context) {
+        // check whether don't show shown is opted
+        if (ZulipApp.get().getSettings().getBoolean(Constants.DONT_SHOW_APP_UPDATE_DIALOG, false)) {
+            return;
+        }
+
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
         builder.setPositiveButton(R.string.update, new DialogInterface.OnClickListener() {
+            @Override
             public void onClick(DialogInterface dialog, int id) {
                 // open new app page on play store
                 Intent intent = new Intent(Intent.ACTION_VIEW,
@@ -23,15 +30,23 @@ public class ShowAppUpdateDialog {
             }
         });
         builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
             public void onClick(DialogInterface dialog, int id) {
 
             }
         });
+
+        builder.setNeutralButton(R.string.dont_show_again, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ZulipApp.get().setDontShowAppDialog(true);
+            }
+        });
+
         AlertDialog dialog = builder.create();
         dialog.setMessage("This project is deprecated in favor of React Native app, " +
                 "which supports both iOS and Android. Click update to switch to the new app.");
 
         dialog.show();
-
     }
 }
